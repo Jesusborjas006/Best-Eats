b5d3aa9d0e46ab5f14248b5b59458628545e4d47
Jesus Borjas <jesusbor006@gmail.com>
2023-08-09 14:53:27 -0500

    Add headline cards

---
+++ src/components/HeadlineCards.js
@@ -0,0 +1,52 @@
+const HeadlineCards = () => {
+  return (
+    <div className="max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6">
+      <div className="rounded-xl relative">
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">Sun's Out, BOGO's Out</p>
+          <p className="px-2">Through 8/26</p>
+          <button className="border border-white bg-white text-black mx-2 absolute bottom-4 rounded-full py-1 px-5">
+            Order Now
+          </button>
+        </div>
+        <img
+          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://images.pexels.com/photos/718742/pexels-photo-718742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="food"
+        />
+      </div>
+      <div className="rounded-xl relative">
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">New Restaurants</p>
+          <p className="px-2">Added Daily</p>
+          <button className="border border-white bg-white text-black mx-2 absolute bottom-4 rounded-full py-1 px-5">
+            Order Now
+          </button>
+        </div>
+        <img
+          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://images.pexels.com/photos/718742/pexels-photo-718742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="food"
+        />
+      </div>
+      <div className="rounded-xl relative">
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">
+            We Deliver Desserts Too
+          </p>
+          <p className="px-2">Tasty Treats</p>
+          <button className="border border-white bg-white text-black mx-2 absolute bottom-4 rounded-full py-1 px-5">
+            Order Now
+          </button>
+        </div>
+        <img
+          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://images.pexels.com/photos/718742/pexels-photo-718742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="food"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default HeadlineCards;
